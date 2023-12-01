5572e3667a284ab81c1becdc3fad61c5f440c439
Yuki Fujita <uni22yu@gmail.com>
2023-12-02 03:25:28 +0900

    ABC039

---
+++ ABC/A/ABC039.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a*b*2 + b*c*2 + c*a*2 << endl;
+    return 0;
+}
+
